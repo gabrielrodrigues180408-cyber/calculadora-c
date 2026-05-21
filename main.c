883e53d70d492104f98309ae176020d59ad3edda
Gabriel Rodrigues <gabriel.rodrigues180408@gmail.com>
2026-05-21 15:41:18 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n1, n2;
+
+    printf("Digite um numero: ");
+    scanf("%d", &n1);
+
+    printf("Digite outro numero: ");
+    scanf("%d", &n2);
+
+    printf("Soma = %d", n1 + n2);
+
+    return 0;
+}
